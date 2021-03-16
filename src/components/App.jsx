1aedc7292ac0b734534bad872b36c0c448d79205
Manish Ranjan Behera <manish12042000@gmail.com>
2021-03-16 18:58:45 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import EmojiCards from "./EmojiCards";
+import emojipedia from "../emojipedia";
+
+function createEmojis(emojiCard) {
+  return (
+    <EmojiCards
+      key={emojiCard.id}
+      emoji={emojiCard.emoji}
+      name={emojiCard.name}
+      meaning={emojiCard.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createEmojis)};</dl>
+    </div>
+  );
+}
+
+export default App;
